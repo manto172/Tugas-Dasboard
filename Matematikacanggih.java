a21262c758c39f12f6fea6536a2339b56fd9113d
manto172 <mantotaak@gmail.com>
2024-11-24 23:23:34 +0700

    Matematikacanggih.java

---
+++ Matematikacanggih.java
@@ -0,0 +1,14 @@
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematika;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class matematikacanggih extends matematika1 {
+    public int modulus(int nilai1, int nilai2){
+        return nilai1 % nilai2;
+    }
+}
